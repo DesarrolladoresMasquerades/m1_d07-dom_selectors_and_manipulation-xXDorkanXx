e176f461fb3118cdf4b71c135e2c2b3a7bd258d2
Fer Sánchez <sangarfer@gmail.com>
2022-01-25 16:40:56 +0100

    02-dom-manipulation class done

---
+++ 02-dom_manipulation/scripts/script.js
@@ -0,0 +1,48 @@
+console.log("JS Loaded");
+
+const rootDiv = document.getElementById("cappuccino");
+/*
+// create and populate/fill the element
+const rootH2 = document.createElement("h2");
+rootH2.innerText = "To-do list";
+rootH2.classList.add("main-subtitle");
+
+// attach it to a parent
+rootDiv.appendChild(rootH2);
+*/
+
+// create button element
+const actionButton = document.createElement("button");
+actionButton.innerText = "Add to-do";
+actionButton.classList.add("action-btn");
+
+function actionOnClick(event){
+    console.log("button clicked", event);
+};
+
+function addToDoItem(text){
+    const newToDo = document.createElement("li");
+    newToDo.innerText = text;
+    document.getElementById("items-list").appendChild(newToDo);
+};
+
+actionButton.addEventListener(
+    "click",
+    (event)=> {   //actionOnClick
+        // console.log("button clicked", event);
+
+        // we select an element from inside the action
+        
+        const inputToDo = document.querySelector("[name='to-do-item']");
+        event.addToDoItem(inputToDo.value);
+        inputToDo.focus();
+        inputToDo.value = "";
+
+    } 
+);
+
+rootDiv.appendChild(actionButton);
+
+
+
+
