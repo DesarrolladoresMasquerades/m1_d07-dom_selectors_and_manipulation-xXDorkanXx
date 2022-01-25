9764d7c3eb73c28a7a3bd2834bb85b8607429d92
Fer Sánchez <sangarfer@gmail.com>
2022-01-25 13:22:29 +0100

    01-exe-dom_selector done

--- exercises/01-exe-dom_selectors/scripts/script.js
+++ exercises/01-exe-dom_selectors/scripts/script.js
@@ -1,48 +1,55 @@
 // 1: Get the node with the  main title
 
-// ... your code here
+console.log(document.querySelector("h1"));
 
 // => <h1>Fruits and veggies</h1>
 // ----------------------------------------------
 
 // 2: Get the main title text
 
-// ... your code here
+console.log(document.querySelector("h1").innerText);
 
 // => Fruits and veggies
 // ----------------------------------------------
 
 // 3: Get all the nodes with the fruit items
 
-// ... your code here
+console.log(document.getElementsByClassName("fruit-item"))
 
 // => HTMLCollection(4) [li.fruit-item, li.fruit-item, li.fruit-item, li.fruit-item]
 // ----------------------------------------------
 
 // 4: Get all the veggie items - the text
 
-// ... your code here
+const arrVeggies = Array.from(document.getElementsByClassName("veggie-item"));
+
+arrVeggies.forEach(element => console.log(element.innerText));
 
 // Broccoli Celery Potato Spinach
 // ----------------------------------------------
 
 // 5: Get the subtitle in the veggies section
 
-// ... your code here
+console.log(document.querySelector(".list-veggies h2").innerText);
 
 // => Veggies
 // ----------------------------------------------
 
 // 6: Add an ID 'fruit-title' to the first subtitle (in the Fruit section)
 
-// ... your code here
+document.querySelector(".list-fruits h2").id = "fruit-title";
+
+console.log(document.querySelector(".list-fruits h2").id);
 
 // => fruit-title
 // ----------------------------------------------
 
 // 7: Add a class 'best-fruit' to the 1st and the 3rd fruit
 
-// ... your code here
+document.getElementsByClassName("fruit-item")[0].classList.add("best-fruit");
+document.getElementsByClassName("fruit-item")[2].classList.add("best-fruit");
+
+console.log(document.getElementsByClassName("best-fruit"));
 
 
 // => <li class="fruit-item best-fruit">Pear</li>
@@ -51,20 +58,26 @@
 
 // 8: Remove class 'veggies' from the unordered list of veggies
 
-// ... your code here
+document.querySelector(".list-veggies ul").className = "" //classList.remove("veggies");
 
-// => <ul>...</ul>
+console.log(document.querySelector(".list-veggies ul"));
+
+// => <ul class>...</ul>
 // ----------------------------------------------
 
 // 9: Add a class 'veggie-love' to all the veggies
 
-// ... your code here
+arrVeggies.forEach(element => element.className += " veggie-love"); //arrVeggies.forEach(element => element.classList.add("veggie-love"));
+
+arrVeggies.forEach(element => console.log(element));
 
 // => <li class="veggie-item veggie-love">Broccoli</li> ...
 // ----------------------------------------------
 
 // 10: Remove class 'veggie-item' from 'spinach'
 
-// ... your code here
+document.getElementsByClassName("veggie-love")[3].classList.remove("veggie-item");
+
+console.log(document.getElementsByClassName("veggie-love")[3]);
 
 // => ... <li class="veggie-love">Spinach</li>
